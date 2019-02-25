086621281cfc71ed082da35dcd73c3a6c76747c6
Tifider <Tifider@gmail.com>
2019-02-25 18:03:39 +0300

    Made it modular

---
+++ gateway/src/main/java/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class GatewayApplication {
+    public static void main(String[] args) {
+    }
+}
