636d6e85f857e1bbc2f334abdc7dc02cbb6ede9f
Roman_Selin <Tifider@gmail.com>
2019-03-05 15:30:50 +0300

    "Added book-service and rating modules"

---
+++ book-service/src/main/java/BookServiceApplication.java
@@ -0,0 +1,24 @@
+import POJO.Book;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@RequestMapping("/books")
+public class BookServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookServiceApplication.class, args);
+    }
+
+    private List<Book> bookList = Arrays.asList(
+            new Book(1L, "Something", "Vasya Pupkind"),
+            new Book(20L, "Something something", "Alexandr Nevsky")
+    );
+}
